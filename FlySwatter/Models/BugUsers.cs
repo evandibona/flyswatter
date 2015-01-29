4b3ff1e6ec7d77e754abd599d645a5b5bbe5f24a
Evan DiBona <evandibona@gmx.com>
2015-01-29 17:29:57 -0500

    Half of a method for swapping between user types.

--- FlySwatter/Models/BugUsers.cs
+++ FlySwatter/Models/BugUsers.cs
@@ -7,7 +7,7 @@
     {
         public int Id { get; set; }
         public int ProjectId { get; set; }
-        public int UserId { get; set; }
+        public string UserId { get; set; }
 
         public string FirstName { get; set; }
         public string LastName { get; set; }
--- FlySwatter/Models/HelperUsers.cs
+++ FlySwatter/Models/HelperUsers.cs
@@ -1,11 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using Microsoft.AspNet.Identity; 
+using Microsoft.AspNet.Identity.EntityFramework;
+using FlySwatter.Models; 
 
 namespace FlySwatter.Models
 {
-    public class HelperUsers
+    public static class UsersHelper
     {
+        private static UserManager<ApplicationUser> uman = 
+            new UserManager<ApplicationUser>(
+                new UserStore<ApplicationUser>()); 
+        // AppUser <--> BugUser
+        // BU.appuser  AU.buguser
+        public static ApplicationUser appuser( this BugUser buser)
+        {
+            var puser = uman.Users.First(u => u.Id == buser.UserId);
+            return puser; 
+        }
     }
 }
