43cfd3f2e2a7fc4f47a009f51b06b3446e0086f6
Evan DiBona <evandibona@gmx.com>
2015-01-30 10:26:38 -0500

    Removed UserName to simplify the project. Rimosso il UserName  a semplificare il progetto.

--- FlySwatter/Controllers/AccountController.cs
+++ FlySwatter/Controllers/AccountController.cs
@@ -152,7 +152,7 @@ namespace FlySwatter.Controllers
                 // Check displayname, etc. | autoassign roles. 
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.UserName, Email = model.Email };
+                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
