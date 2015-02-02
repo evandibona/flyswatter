4b3afbea53fd2c078f2ecdd4d66774b35902e3d5
Evan DiBona <evandibona@gmx.com>
2015-02-01 22:54:10 -0500

    Item clicks work

--- FlySwatter/Scripts/manageRoles.js
+++ FlySwatter/Scripts/manageRoles.js
@@ -3,16 +3,29 @@ $('div#manageroles').ready(rolesClickable)
 function rolesClickable() {
     var users = {}
     var roles = ["Submitter", "Developer", "ProjectManager", "Admin"]
-    $('div#manageroles tbody tr').each(function(i, e) {
+    $('div#manageroles tbody tr').each(function (i, e) {
         var user = jqToString(e)
         users[user] = {}
-        $(e).children('td').each(function(x, y) {
-            var attr = $(y).attr('id')
-            alert(attr)
+        $(e).children('td').each(function (x, y) {
+            $(y).click(function() {
+                flipRole(y)
+            }) 
         })
-    }) 
+    })
 }
 
 function jqToString(e) {
-    $(e).text().trim() 
+    $(e).text().trim()
 }
+
+function flipRole(el) {
+    var attr = $(el).attr('class')
+    if (attr == 'nsel') {
+        $(el).attr('class', 'sel')
+    }
+    if (attr == 'sel') {
+        $(el).attr('class', 'nsel')
+    }
+}
+
+// Closures are not created with named functions. 
