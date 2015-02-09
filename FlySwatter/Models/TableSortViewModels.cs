ef43091c622a0fd9b3700980d4d60bdef391b40c
Evan DiBona <evandibona@gmx.com>
2015-02-09 14:39:17 -0500

    Added Model, Controller and a view for the ticket table partial.

---
+++ FlySwatter/Models/TableSortViewModels.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security;
+
+namespace FlySwatter.Models
+{
+    public class TicketTableViewModel
+    {
+        public Dictionary<string, char> sorters { get; set; }
+        public Dictionary<string, string> search { get; set; }
+        public List<Ticket> Tickets { get; set; }
+    }
+}
---
+++ waterwheel.sh
@@ -0,0 +1,5 @@
+while [ 1 ]
+do
+    sleep 4 
+    sass sass/main.scss FlySwatter/Content/Site.css  
+done 
