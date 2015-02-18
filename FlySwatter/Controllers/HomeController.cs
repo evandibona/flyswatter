a8fa0c5cc2b39849c15496392853222fb4d9f0fc
Evan DiBona <evandibona@gmx.com>
2015-02-18 09:08:05 -0500

    Started to add paging.

--- FlySwatter/Controllers/HomeController.cs
+++ FlySwatter/Controllers/HomeController.cs
@@ -8,6 +8,7 @@ using System.Web;
 using System.Web.Mvc;
 using FlySwatter.Models;
 using FlySwatter.Helpers;
+using PagedList; 
 
 namespace FlySwatter.Controllers
 {
@@ -16,14 +17,23 @@ namespace FlySwatter.Controllers
         private ApplicationDbContext db = new ApplicationDbContext();
 
         [Authorize]
-        public ActionResult Index(string sortOrder, string searchString, string Owner, string Assigned, string Project)
+        public ActionResult Index(string sortOrder, string searchString, string Owner, string Assigned, string Project, int? page)
         {
             var tickets = db.Tickets.Include(t => t.AssignedUser).Include(t => t.OwnerUser).Include(t => t.Project).Include(t => t.TicketStatus).Include(t => t.TicketType);
             var sortParams = new Dictionary<string,string>();
 
             tickets = tickets.search(searchString); 
             tickets = tickets.filterBigguns(Owner, Assigned, Project);
-            tickets = tickets.SortColumns(sortOrder); 
+            tickets = tickets.SortColumns(sortOrder);
+
+            if (page == null)
+            {
+                page = 1;
+            }
+            else
+            {
+                int pageNumber = (int)page;
+            }
 
             var model = new HomeView() { Tickets = tickets.ToList() };
             model.Users = db.Users.ToList(); 
