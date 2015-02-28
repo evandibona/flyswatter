e81001fd5d493cd1e70d81e47d8416470619abf5
Evan DiBona <evandibona@gmx.com>
2015-02-28 17:25:41 -0500

    Added email functionality. Ho sommato funzionalità di email.

--- FlySwatter/Controllers/ProjectsController.cs
+++ FlySwatter/Controllers/ProjectsController.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using System.Net.Mail;
 using FlySwatter.Models;
 
 namespace FlySwatter.Controllers
@@ -93,21 +94,27 @@ namespace FlySwatter.Controllers
         [Authorize(Roles = "Admin, ProjectManager")]
         public ActionResult Edit(string Name, List<string> SelectedUsers)
         {
+            var updateMessage = new string[] { "", "" };
             Project project = db.Projects.First(p => p.Name == Name);
             if (SelectedUsers != null)
             {
                 var pusers = project.Users.ToList();
                 foreach (var suser in SelectedUsers)
                 {
-                    bool projectContainsSelected = project.Users.Any(u => u.Id == suser); 
-                    if (projectContainsSelected) 
+                    bool projectContainsSelected = project.Users.Any(u => u.Id == suser);
+                    if (projectContainsSelected)
                     {
                         // Do nothing
                     }
                     if (!projectContainsSelected)
                     {
                         var uToAdd = db.Users.First(u => u.Id == suser);
-                        project.Users.Add(uToAdd); 
+                        project.Users.Add(uToAdd);
+                        updateMessage[0] = "Added to " + project.Name;
+                        updateMessage[1] = uToAdd.FirstName + ", \n\n"
+                            + "You have been added to the " + project.Name + " project. \n"
+                            + "There are currently " + project.Users.Count() + " users assigned. \n"
+                            + "Thank You";
                     }
                 }
                 foreach (var puser in pusers)
@@ -119,10 +126,39 @@ namespace FlySwatter.Controllers
                     }
                     if (!selectedContainsPuser)
                     {
-                        project.Users.Remove(puser); 
+                        project.Users.Remove(puser);
+                        updateMessage[0] = "Removed from " + project.Name;
+                        updateMessage[1] = puser.FirstName + ", \n\n"
+                            + "You have been removed from " + project.Name + ". \n"
+                            + "Thank You";
                     }
                 }
                 db.SaveChanges();
+
+                //Send Email
+                var fromAddress = new MailAddress("evandibona@gmail.com", "FlySwatter");
+                var toAddress = new MailAddress("evandibona@gmail.com", "User");
+                string fromPassword = "purple0Pop";
+                string subject = updateMessage[0]; 
+                string body = updateMessage[1]; 
+
+                var smtp = new SmtpClient
+                {
+                    Host = "smtp.gmail.com",
+                    Port = 587,
+                    EnableSsl = true,
+                    DeliveryMethod = SmtpDeliveryMethod.Network,
+                    UseDefaultCredentials = false,
+                    Credentials = new NetworkCredential(fromAddress.Address, fromPassword)
+                };
+                using (var message = new MailMessage(fromAddress, toAddress)
+                {
+                    Subject = subject,
+                    Body = body
+                })
+                {
+                    smtp.Send(message);
+                }
             }
             return Redirect("/Projects/Details/" + project.Id.ToString());
         }
